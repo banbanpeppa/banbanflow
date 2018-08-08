7d3d8ceb3f3910aa8db4e9fb7b7871179b2e89f8
陈志凌-chenzhiling01 <chenzhiling01@game.ntes>
2018-08-08 14:16:31 +0800

    model serve and script it

--- script/kubeflow-introduction/tensorflow-model-train.sh
+++ script/kubeflow-introduction/tensorflow-model-train.sh
@@ -4,16 +4,21 @@ set -xe
 uuid=`uuidgen`
 uuid=${uuid:0:8}
 
-if [ ! -d "/tmp/model-train" ]; then
+if [ ! -d "/tmp/model-train-serve" ]; then
 	mkdir -p /tmp/model-train
 fi
 
-MODEL_TRANING_YAML_TMP=/tmp/model-train/training-${uuid}.yaml
+MODEL_SERVE_YAML_TMP=/tmp/model-train-serve/serve-${uuid}.yaml
+MODEL_TRANING_YAML_TMP=/tmp/model-train-serve/training-${uuid}.yaml
 
-curl -sSL https://raw.githubusercontent.com/banbanandroid/banbanflow/master/script/kubeflow-introduction/tensorflow-model.yaml > ${MODEL_TRANING_YAML_TMP}
+curl -sSL "https://raw.githubusercontent.com/banbanandroid/banbanflow/master/script/kubeflow-introduction/mode-serve.yaml" > ${MODEL_SERVE_YAML_TMP}
+curl -sSL "https://raw.githubusercontent.com/banbanandroid/banbanflow/master/script/kubeflow-introduction/tensorflow-model.yaml" > ${MODEL_TRANING_YAML_TMP}
 
+sed -i "s/uuidgen/${uuid}/g" ${MODEL_SERVE_YAML_TMP}
 sed -i "s/uuidgen/${uuid}/g" ${MODEL_TRANING_YAML_TMP}
 
-kubectl create -f ${MODEL_TRANING_YAML_TMP}
+kubectl create -f ${MODEL_SERVE_YAML_TMP}
+sleep 1
 
+kubectl create -f ${MODEL_TRANING_YAML_TMP}
 sleep 1
